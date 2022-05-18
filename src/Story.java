a7a5d6a6119289b33a22b7e4a141882d40038506
ProGamer6969 <athenaballensky@gmail.com>
2022-05-18 13:21:29 -0700

    5/17

--- src/Story.java
+++ src/Story.java
@@ -1,6 +1,7 @@
 public class Story {
 
     string usrAnswr;
+    int questionNum;
 
     public static void setUsrAnswr(string usrAnswr) {
         usrAnswr = usrAnswr;
@@ -9,5 +10,45 @@ public class Story {
     public static string getUsrAnswr() {
         return usrAnswr;
     }
+
+    public void usrAnswrReaction() {
+        if (getUsrAnswer().equals("Fight")){
+            if (questionNum == 1) {
+                questionNum++;
+            }
+            if (questionNum == 2) {
+                questionNum++;
+            }
+        }
+        if (getUsrAnswer().equals("Flee")){
+            if (questionNum == 1) {
+                questionNum++;
+            }
+            if (questionNum == 2) {
+                questionNum++;
+            }
+        }
+        if (getUsrAnswer().equals("Negotiate")){
+            if (questionNum == 1) {
+                questionNum++;
+            }
+            if (questionNum == 2) {
+                questionNum++;
+            }
+        }
+        else {// in case user mistypes
+        while (NOT (getUsrAnswer().equals("Fight") ||getUsrAnswer().equals("Flee") || getUsrAnswer().equals("Negotiate")) {
+            setUsrAnswer = kybd.nextLine();
+        } }
+
+        if (usr.Answer.equals("Fight") && playerAttribute[0] > obstacleAttribute[0]) {
+            grade++;
+        }
+        else {
+            grade--;
+        }
+    }
+
+
 
 }
