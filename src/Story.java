3005499c23327ede9b198feaa00ee7488f0b6010
ProGamer6969 <athenaballensky@gmail.com>
2022-05-18 13:35:19 -0700

    5/17

--- src/Story.java
+++ src/Story.java
@@ -42,13 +42,13 @@ public class Story {
             setUsrAnswer = kybd.nextLine();
         } }
 
-        /
-        */if (usr.Answer.equals("Fight") && playerAttribute[0] > obstacleAttribute[0]) {
+
+        if (usr.Answer.equals("Fight") && playerAttribute[0] > obstacleAttribute[0]) {
             grade++;
         }
         else {
             grade--;
-        } */
+        }
     }
 
 
