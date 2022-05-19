e262124b2785126bdb5c1ded38310b92c0b4e391
MeyerTalon <talonmeyer12@gmail.com>
2022-05-19 12:31:25 -0700

    Update

--- src/Encounter.java
+++ src/Encounter.java
@@ -2,26 +2,32 @@ import java.util.Scanner;
 public class Encounter {
     private String approach;
     private String question;
-    private String victory;
+    private String outocome;
+    private boolean win;
     private Obstacle obstacle;
 
     public Encounter() {
         approach = "";
         question = "";
-        victory = "";
         obstacle = new Obstacle();
     }
 
-    public Encounter(String a, String q, String v, Obstacle o) {
+    public Encounter(String a, String q, Obstacle o) {
         approach = a;
         question = q;
-        victory = v;
         obstacle = o;
     }
-
+    public String getOutcome()
+    {
+        String output = new String();
+        if (win) {
+            output = "You defeated " + obstacle.getObstalceName() + ". Congratulations!";
+        }
+        return output;
+    }
     public String toString() {
         String output = new String();
-        output = approach + question + victory;
+        output = approach + question;
         return output;
     }
 }
