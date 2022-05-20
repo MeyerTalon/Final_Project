074b1b1150cfa1d62edf523259ebe549766a1a21
SkylerCrook <jskycrook@gmail.com>
2022-05-20 12:24:54 -0700

    Update

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,34 @@
 import java.util.*;
 public class Driver {
-    public static void main() {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int roll = 0;
+        System.out.print(Dice());
+
+
+
+        System.out.println("Welcome to Thermopylae\nIt is a world filled with.......");
+
+        System.out.println("You can choose to play as a\n1. Hacker\n2. Intern\n3. Debugger\nSelect your choice");
+        if (scan.nextInt() > 0 && scan.nextInt() < 4) {
+            final int character = scan.nextInt;
+        }
+
+
+
+        //Encounter and Obstacle Testing
         int[] ob1arr = {90, 20, 1};
         Obstacle ob1 = new Obstacle(ob1arr, "Trahan");
         Encounter test = new Encounter("A Trahan crosses your path", "Would you like to:\n1. Fight\n2. Flee\n3. Negotiate", ob1);
-        Scanner scan = new Scanner(System.in);
+        System.out.println(test.getApproach() + " " + test.getQuestion());
+        System.out.println("What is your choice");
         int choiceTest = scan.nextInt();
         test.setChoice(choiceTest);
+        System.out.print(test);
+
+    }
+
+    public static int Dice() {
+        return (int) (Math.random() * 6) + 1;
     }
 }
--- src/Story/StoryDriver.java
+++
@@ -1,7 +0,0 @@
-package Story;
-
-public class StoryDriver {
-    public static void main(String[] args) {
-        System.out.println("hi");
-    }
-}
