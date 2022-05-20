6846b11f7e3e2989423364530103c3e39f900a75
MeyerTalon <talonmeyer12@gmail.com>
2022-05-20 12:35:31 -0700

    Update

--- src/InternThatWorksout.java
+++ src/InternThatWorksout.java
@@ -8,7 +8,7 @@ public class InternThatWorksout extends Player {
     public void setPlayerAttributeStatus() {
             playerAttributeStatus[0] = 40; //fight, strength
             playerAttributeStatus[1] = 10; //flee, speed
-            playerAttributeStatus[2] = -10; //negotiate, cunning
+            playerAttributeStatus[2] = 5; //negotiate, cunning
     }
 
             /* Strength, Cunning,  Speed*/
