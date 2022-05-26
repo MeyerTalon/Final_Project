b33fcef8e5bc6bbff467080b85bd4e7f4a94313b
SkylerCrook <jskycrook@gmail.com>
2022-05-26 12:28:05 -0700

    Update

--- src/Display.java
+++ src/Display.java
@@ -1,40 +1,40 @@
-//(fix) Global Class: If___, then set ImageName to ___;
-//(done) Get input from Global Variables (Image Name String)
-//(done) Display image
-
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-
-public class Display {
-    public Display() {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                JFrame frame = new JFrame("Display");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                BufferedImage img = null;
-
-                try {
-                    img = ImageIO.read(getClass().getResource("/" + img /*should look like this: /sad keanu.jpg*/));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    System.exit(1);
-                }
-
-                ImageIcon imgIcon = new ImageIcon(img);
-                JLabel lbl = new JLabel();
-                lbl.setIcon(imgIcon);
-                frame.getContentPane().add(lbl, BorderLayout.CENTER);
-                frame.pack();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-            }
-        });
-    }
-
-    public static void main(String[] args) throws Exception {
-        new Display();
-    }
-}
+////(fix) Global Class: If___, then set ImageName to ___;
+////(done) Get input from Global Variables (Image Name String)
+////(done) Display image
+//
+//import javax.imageio.ImageIO;
+//import javax.swing.*;
+//import java.awt.*;
+//import java.awt.image.BufferedImage;
+//import java.io.IOException;
+//
+//public class Display {
+//    public Display() {
+//        SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//                JFrame frame = new JFrame("Display");
+//                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//                BufferedImage img = null;
+//
+//                try {
+//                    img = ImageIO.read(getClass().getResource("/" + img /*should look like this: /sad keanu.jpg*/));
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                    System.exit(1);
+//                }
+//
+//                ImageIcon imgIcon = new ImageIcon(img);
+//                JLabel lbl = new JLabel();
+//                lbl.setIcon(imgIcon);
+//                frame.getContentPane().add(lbl, BorderLayout.CENTER);
+//                frame.pack();
+//                frame.setLocationRelativeTo(null);
+//                frame.setVisible(true);
+//            }
+//        });
+//    }
+//
+//    public static void main(String[] args) throws Exception {
+//        new Display();
+//    }
+//}
--- src/Global.java
+++ src/Global.java
@@ -3,23 +3,5 @@ public class Global {
 
     public int loc = 0;
 
-    public String Images() {
-        String img = "";
-        if (loc == 1) {
-            img = "horse.png";
-        }
-        if (loc == 2) {
-            img = "New Piskel (7).png";
-        }
-        if (loc == 3) {
-            img = "RubberDucky.png";
-        }
-        if (loc == 4) {
-            img = "Robot1.png";
-        }
-        if (loc == 5) {
-            img = "Angryvirus6.png";
-        }
-        return img;
-    }
+
 }
